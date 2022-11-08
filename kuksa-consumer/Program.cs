9e63be663111caf6e0e64556764a1a1946efe89e
Bastian Burger <baburger@microsoft.com>
2022-11-08 18:56:01 +0100

    Pass data to consumer via standard output

--- kuksa-consumer/Program.cs
+++ kuksa-consumer/Program.cs
@@ -1,51 +1,22 @@
-﻿using System.Net.WebSockets;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Channels;
+﻿using System.Text.Json;
 using Grpc.Core;
 using Grpc.Net.Client;
 using Sdv.Databroker.V1;
 
-var app = WebApplication.CreateBuilder().Build();
-app.UseWebSockets();
+var channel = GrpcChannel.ForAddress("http://localhost:55555");
+var client = new Broker.BrokerClient(channel);
 
-app.Use(async (context, next) =>
+var response = client.Subscribe(request: new SubscribeRequest { Query = "SELECT Vehicle.Chassis.SteeringWheel.Angle,Vehicle.CurrentLocation.Timestamp,Vehicle.Speed" });
+
+while (await response.ResponseStream.MoveNext())
 {
-    if (context.WebSockets.IsWebSocketRequest)
+    var data = JsonSerializer.Serialize(new
     {
-        if (context.WebSockets.IsWebSocketRequest)
-        {
-            using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        SteeringWheelAngle = response.ResponseStream.Current.Fields["Vehicle.Chassis.SteeringWheel.Angle"].Int32Value,
+        VehicleSpeed = response.ResponseStream.Current.Fields["Vehicle.Speed"].FloatValue,
+        Timestamp = response.ResponseStream.Current.Fields["Vehicle.CurrentLocation.Timestamp"].Timestamp
+    });
 
-            var channel = GrpcChannel.ForAddress("http://localhost:55555");
-            var client = new Broker.BrokerClient(channel);
-
-            var response = client.Subscribe(request: new SubscribeRequest { Query = "SELECT Vehicle.Chassis.SteeringWheel.Angle,Vehicle.CurrentLocation.Timestamp,Vehicle.Speed" });
-
-            while (await response.ResponseStream.MoveNext())
-            {
-                await webSocket.SendAsync(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(new
-                {
-                    SteeringWheelAngle = response.ResponseStream.Current.Fields["Vehicle.Chassis.SteeringWheel.Angle"].Int32Value,
-                    VehicleSpeed = response.ResponseStream.Current.Fields["Vehicle.Speed"].FloatValue,
-                    Timestamp = response.ResponseStream.Current.Fields["Vehicle.CurrentLocation.Timestamp"].Timestamp
-                })),
-                WebSocketMessageType.Text,
-                true,
-                CancellationToken.None);
-
-                Console.WriteLine(response.ResponseStream.Current);
-            }
-        }
-        else
-        {
-            await next(context);
-        }
-    }
-    else
-    {
-        context.Response.StatusCode = StatusCodes.Status400BadRequest;
-    }
-});
-
-app.Run();
+    // Pass the data to the consuming application via standard output
+    Console.WriteLine(data);
+}
